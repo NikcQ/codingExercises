ab461cc29690e1709ae784defd33fa5e67432c3c
NikcQ <nikoo576@hotmail.com>
2025-04-08 00:06:44 -0500

    chore(global): commented out grid challenge and added a new fun challenge for a friend

--- Hackerrank/GridChallenge.ts
+++ Hackerrank/GridChallenge.ts
@@ -1,13 +1,20 @@
+/*
 function gridChallenge(grid: string[]): string {
     
     let comparator: string[] = []
     let organised: string[] = [];
+    let organisedFlag : Boolean = false;
 
     for (let i = 0; i < grid.length; i++) {
         
         organised.push(grid[i].split("").sort().join(""))
-        comparator.push(organised[i].split("")[0])
+        comparator.push(organised.split("")[0])
+    }
 
+
+    for (let i = 0; i < comparator.length; i++) {
+        const element = array[i];
+        
     }
 
     console.log(organised)
@@ -19,3 +26,4 @@ function gridChallenge(grid: string[]): string {
 
 console.log(gridChallenge(['kc','iu']))
 console.log(gridChallenge(['uxf','vof','hmp']))
+*/
---
+++ various/LinkedinProblem.ts
@@ -0,0 +1,26 @@
+//I saw this one on LinkedIn and truly wanted to give it a go
+// Challenge time: 25 Minutes
+
+//input is: "aaaabbbcca"
+//output is: ["("a",4)", "("b",3)", "("c",2)", "("a",1)", ]
+
+function linkedIn(input: String): String[]{
+
+    if(!input){
+        return[]
+    }
+
+    let counterArray: String[] = [];
+    let letterArray = [];
+    let array = input.split("");
+    let counter = 0;
+    
+
+    for (let i = 0; i < array.length; i++) {
+
+        letterArray[letterArray.length-1] === array[i]
+        
+    }
+
+    return []
+}
